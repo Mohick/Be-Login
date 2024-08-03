0459f2a42ff801ab08cd39d93fc9fe8d618dbb08
Mohick <haokietks7@gmail.com>
2024-08-04 00:40:07 +0700

    test

--- src/Config/Session Cookes/Session Cookes.js
+++ src/Config/Session Cookes/Session Cookes.js
@@ -5,7 +5,7 @@ const cookieSession = require('cookie-session')
 const configSessionCookies = (app) => {
     app.use(cookieSession({
         name: 'session',
-        secret:     ,
+        secret: process.env.EXPRESS__SECRET__KEY,
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 ngày
         secure: true, // Chỉ gửi cookie qua HTTPS trong môi trường sản xuất
         httpOnly: true,
